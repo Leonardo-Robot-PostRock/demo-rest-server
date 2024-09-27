10b24c5c222e9c7b955b244be7dcb570ed6b2fa7
Leonardo <leonardo38361@gmail.com>
2024-09-26 23:34:04 -0300

    validación de roles contra db

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validateFields } = require('../middlewares/validate-fields');
+const Role = require('../models/role');
 
 const {
     usersGet,
@@ -18,7 +19,13 @@ router.post('/', [
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe ser más de 6 letras').isLength({ min: 6 }),
     check('email', 'Correo no válido').isEmail(),
-    check('role', 'No es un rol válido').isIn(['USER_ROLE', 'ADMIN_ROLE']),
+    check('role').custom(async (role = '') => {
+        const roleExists = await Role.findOne({ role });
+        console.log('Resultado: ', roleExists)
+        if (!roleExists) {
+            throw new Error(`The ${role} is not in the database`)
+        }
+    }),
     validateFields
 ],
     usersPost
