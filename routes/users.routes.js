c3daa1bafa0f15512d6c3112c99258a227494150
Leonardo <leonardo38361@gmail.com>
2024-10-04 00:53:34 -0300

    paso de la función al middleware

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validateFields } = require('../middlewares/validate-fields');
-const { isRoleValid } = require('../helpers/db-validators');
+const { isRoleValid, checkEmailExists } = require('../helpers/db-validators');
 
 const {
     usersGet,
@@ -18,7 +18,7 @@ router.get('/', usersGet);
 router.post('/', [
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe ser más de 6 letras').isLength({ min: 6 }),
-    check('email', 'Correo no válido').isEmail(),
+    check('email', 'Correo no válido').custom(checkEmailExists).isEmail(),
     check('role').custom(isRoleValid),
     validateFields
 ],
