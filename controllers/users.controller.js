435b8edd567a49bd2032310110ce078d15061411
Leonardo <leonardo38361@gmail.com>
2024-10-06 01:28:46 -0300

    ADD: to exclude id

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -33,7 +33,7 @@ const usersPost = async (req, res = response) => {
 const usersPut = async (req, res = response) => {
     const { id } = req.params;
 
-    const { password, google, email, ...rest } = req.body;
+    const { _id, password, google, email, ...rest } = req.body;
 
     if (password) {
         const salt = bcrypt.genSaltSync();
