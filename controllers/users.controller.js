4098293b0ea4d79315a807a7cac92db068bb2b54
Leonardo <leonardo38361@gmail.com>
2024-10-09 18:34:53 -0300

    REMOVE: comment

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -65,8 +65,6 @@ const usersDelete = async (req, res = response) => {
 
     const { id } = req.params;
 
-    // const user = await User.findByIdAndDelete(id);
-
     const user = await User.findByIdAndUpdate(id, { state: false })
 
     res.json({
